70711b8f57712ee7b5b220b4c97c43635ba8bee0
Marmik Jethwa <marmikjethwa@gmail.com>
2025-08-29 12:36:16 +0530

    final change

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const path=require('path');
+const {fileURLToPath}=require('url');
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'public')))
+app.get('/', (req, res) => {
 
+    res.sendFile(path.join(__dirname, 'index.html'));
+
+});
 app.post('/bfhl', (req, res) => {
     try {
         const { data } = req.body;
@@ -78,5 +85,3 @@ app.post('/bfhl', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
 });
-
-module.exports = app;
