b20773cbeb2720fbb748132a894546ca62aa115b
Marmik Jethwa <marmikjethwa@gmail.com>
2025-08-29 12:05:30 +0530

    Fix Vercel routing for HTML and API

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.use(express.static(__dirname));
 
 app.post('/bfhl', (req, res) => {
     try {
